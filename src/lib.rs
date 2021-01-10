734e4eaef10a2344c339cb9c32594f98317501ad
Harald Held <harald.held@gmail.com>
2021-01-10 14:09:35 +0100

    Also export steepest descent algorithm.

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ mod step_size_control;
 mod vec_utils;
 
 pub use nlp::{dump_nlp, NlpInfo, VariableBounds, NLP};
-pub use optimizer::Bfgs;
+pub use optimizer::{Bfgs, SteepestDescent};
 pub use output::StdoutLogger;
 pub use solver::{AugmentedLagrangianConstraintHandler, BarrierBoundsHandler, Solver};
 pub use step_size_control::ArmijoGoldsteinRule;
